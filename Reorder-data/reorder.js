10253213c105483e49b12394333e2d58d34acc78
matthewweichen <mattchen1021@gmail.com>
2020-10-22 19:54:30 -0700

    only need to sort by alphabetical order

---
+++ Reorder-data/reorder.js
@@ -0,0 +1,31 @@
+//input: array
+//output: array sorted
+
+
+//iterate through array
+//first index is a log (string)
+//make string into an array
+//iterate through string array
+ //check index 1 if number or letter
+ //if letter push into storage array for letters
+//if number push into storage array for numbers
+//sort storage array for letters
+//combine arrays
+
+var reorderLogFiles = function (logs) {
+  const storageArr = [];
+  const numArr = [];
+  const letterArr = [];
+  for(let i = 0; i < logs.length; i++){
+    const tempArr = logs[i].split(' ');
+    if(isNaN(tempArr[1])){
+      letterArr.push(logs[i]);
+    } else {
+      numArr.push(logs[i]);
+    }
+  }
+  storageArr.push(letterArr, numArr); //pushing arrays into array.
+  const flatArr = storageArr.flat();
+
+  return flatArr;
+};
