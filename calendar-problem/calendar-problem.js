49b33d19d50aaacc9031a590b4c1f0c0eda33246
matthewweichen <mattchen1021@gmail.com>
2020-11-04 02:09:08 -0800

    reviewed

--- calendar-problem/calendar-problem.js
+++ calendar-problem/calendar-problem.js
@@ -1,17 +1,3 @@
-//input array of time ranges
-//output array of condensed ranges
-//comebine times
-//if start time overlaps with another end time combine times
-//1 - 30
-//2 - 60
-
-//iterate through array
-//sort array -> sort my start time
-//if endtime in object is less add to array
-//else end time is same or greater combine both objects
-//to combine - get start time if times[i] and add endtime of times[i+1]?
-//return times
-
 function mergeRanges(meetings){
   //create a deep copy of the meetings array
   const meetingsCopy = JSON.parse(JSON.stringify(meetings));
@@ -27,7 +13,6 @@ function mergeRanges(meetings){
   for(let i = 0; i < sortedMeetings.length; i++){
     const currentMeeting = sortedMeetings[i];
     const lastMergedMeeting = mergedMeetings[mergedMeetings.length - 1];
-
 
     //if the current meeting overlaps with the last merged meeting, use the
     //later end time of the two
