43300cbcf3bbaea2d6f4032b1395b05ed03e514b
matthewweichen <mattchen1021@gmail.com>
2020-11-15 16:39:55 -0800

    completed

--- older-than-mr/older-than-me.js
+++ older-than-mr/older-than-me.js
@@ -5,7 +5,13 @@ class Person {
 	}
 
 	compareAge(other) {
-		// Write code here!
+		if(Person.age > other.age){
+      return `${Person.name} is younger than me.`
+    } else if (Person.age < other.age) {
+      return `${other.name} is older than me.`
+    } else {
+      return `${other.name} is the same age as me.`
+    }
 
 	}
 }
