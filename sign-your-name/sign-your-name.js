8814fa1fa53d0cbe2ce910e8027a1edf24662c92
matthewweichen <mattchen1021@gmail.com>
2020-11-25 20:21:25 -0800

    completed sign-your-name

---
+++ sign-your-name/sign-your-name.js
@@ -0,0 +1,9 @@
+function signYourName(obj) {
+  Object.seal(obj)
+  // DON'T CHANGE OR REMOVE THE LINES BELOW
+  obj.yourSignature = "Whatever";
+	obj.spouseSignature = "Nice Try"
+  return obj;
+}
+
+//
