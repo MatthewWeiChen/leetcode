b378fd8e57bf0b771be4f7e186eeed032f52de34
matthewweichen <mattchen1021@gmail.com>
2020-12-17 18:32:12 -0800

    completed sales by match

--- sales-by-match/sales-by-match.js
+++ sales-by-match/sales-by-match.js
@@ -1,45 +1,55 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', _ => {
-    inputString = inputString.replace(/\s*$/, '')
-        .split('\n')
-        .map(str => str.replace(/\s*$/, ''));
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .replace(/\s*$/, "")
+    .split("\n")
+    .map((str) => str.replace(/\s*$/, ""));
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 // Complete the sockMerchant function below.
 function sockMerchant(n, ar) {
-
-
+  const arrSort = ar.sort();
+  let counter = 0;
+  for (let i = 0; i < arrSort.length; i++) {
+    if (arrSort[i] === arrSort[i + 1]) {
+      counter++;
+      i++;
+    }
+  }
+  return counter;
 }
 
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    const n = parseInt(readLine(), 10);
+  const n = parseInt(readLine(), 10);
 
-    const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
+  const ar = readLine()
+    .split(" ")
+    .map((arTemp) => parseInt(arTemp, 10));
 
-    let result = sockMerchant(n, ar);
+  let result = sockMerchant(n, ar);
 
-    ws.write(result + "\n");
+  ws.write(result + "\n");
 
-    ws.end();
+  ws.end();
 }
