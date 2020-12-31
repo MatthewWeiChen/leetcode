8f27ca59a4d943b41360986357828452aad88126
matthewweichen <mattchen1021@gmail.com>
2020-12-30 17:17:56 -0800

    completed

---
+++ isValidSubsequence/is-valid.js
@@ -0,0 +1,12 @@
+function isValidSubsequence(array, sequence) {
+  let sequenceNum = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (sequence[sequenceNum] === array[i]) {
+      sequenceNum++;
+      if (sequenceNum === sequence.length) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
